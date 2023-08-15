f3015df4c289a300dbaed5ddcc9d556667676123
Edward Bagge <137063180+edwardbagge@users.noreply.github.com>
2023-08-15 15:33:45 +0300

    Update data_handling.js

--- data_handling.js
+++ data_handling.js
@@ -6,27 +6,64 @@
  */
 
 
-fetch('projects.json').then(response => response.json()).then(data =>
+function getLanguageSkills()
 {
-    const projectList = document.getElementById('project-list');
-
-    data.forEach(project =>
+    fetch('language_skills.json').then(response => response.json()).then(data =>
     {
-        const projectItem = document.createElement('div');
-        projectItem.classList.add('project-item');
+        const languageSkillsContainer = document.getElementById('languageSkills');
+        let content = '';
 
-        const projectTitle = document.createElement('h4');
-        projectTitle.textContent = project.title;
+        data.forEach(item =>
+        {
+            if (item.category === 'Language Skills')
+            {
+                content += '<p><b>' + item.category + ':</b></p>';
+                content += '<ul>';
 
-        const projectDescription = document.createElement('p');
-        projectDescription.textContent = project.description;
+                item.languages.forEach(language =>
+                {
+                    content += '<li><p>' + language.language + ' (' + language.fluency + ')</p></li>';
+                });
 
-        projectItem.appendChild(projectTitle);
-        projectItem.appendChild(projectDescription);
-        projectList.appendChild(projectItem);
+                content += '</ul>';
+            }
+        });
+
+        languageSkillsContainer.innerHTML = content;
+    })
+    .catch(error =>
+    {
+        console.error('Error fetching JSON:', error);
     });
-})
-.catch(error =>
+}
+
+
+function getRecentProjects()
 {
-    console.error('Error fetching projects:', error);
-});
+    const dataList = document.getElementById('recentProjectInformation');
+
+    fetch('projects.json').then(response => response.json()).then(jsonData =>
+    {
+        jsonData.forEach(item =>
+        {
+            const listItem = document.createElement('li');
+            const titleHeader = document.createElement('h4');
+            const descriptionPara = document.createElement('p');
+
+            titleHeader.textContent = item.title;
+            descriptionPara.textContent = item.description;
+
+            listItem.appendChild(titleHeader);
+            listItem.appendChild(descriptionPara);
+            dataList.appendChild(listItem);
+        });
+    })
+    .catch(error =>
+    {
+        console.error('Error fetching JSON data:', error);
+    });
+}
+
+
+getLanguageSkills();
+getRecentProjects();
