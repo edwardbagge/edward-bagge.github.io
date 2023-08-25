430e3eded90296ff7f3d07735b7cc6a689b1738a
Edward Bagge <137063180+edwardbagge@users.noreply.github.com>
2023-08-25 14:00:18 +0300

    Add files via upload

---
+++ backend_code/data_handling.js
@@ -0,0 +1,101 @@
+export class DataHandler
+{
+    async getData()
+    {
+        try
+        {
+            const response = await fetch('../resources/personal_data.json');
+            const data = await response.json();
+
+            this.fetchCommonSkills(data["commonSkills"]);
+        }
+
+        catch (error)
+        {
+            console.error('Error fetching JSON:', error);
+        }
+    }
+
+
+    fetchCommonSkills(sectionData)
+    {
+        const container = document.getElementById(sectionData["id"]);
+
+        for (const item of sectionData["dataList"])
+        {
+            const section = document.createElement('section');
+            const h4 = document.createElement('h4');
+            const ul = document.createElement('ul');
+
+            const category = item["category"];
+            const skills = item["skills"];
+
+            skills.forEach(skill =>
+            {
+                const li = document.createElement('li');
+                li.innerHTML = '<p>' + skill + '</p>';
+                ul.appendChild(li);
+            });
+
+            h4.innerHTML = category;
+
+            section.appendChild(h4);
+            section.appendChild(ul);
+            section.classList.add('skill_box');
+
+            container.appendChild(section);
+        }
+    }
+
+
+    activateContactButtons()
+    {
+        const showContactBox = document.getElementById("showContactInformation");
+        const closeContactBox = document.getElementById("hideContactInformation");
+        const contactBox = document.getElementById("contactBox");
+
+        showContactBox.addEventListener("click", () =>
+        {
+            contactBox.style.display = "block";
+        });
+
+        closeContactBox.addEventListener("click", () =>
+        {
+            contactBox.style.display = "none";
+        });
+    }
+
+
+    async getTexts(file_path, text_id)
+    {
+        const scriptUrl = new URL(import.meta.url);
+        const filePath = new URL(file_path, scriptUrl).toString();
+
+        let container = document.getElementById(text_id);
+
+        let rawFile = new XMLHttpRequest();
+        rawFile.open("GET", filePath, true);
+        rawFile.onreadystatechange = function ()
+        {
+            if (rawFile.readyState === 4 && (rawFile.status === 200 || rawFile.status === 0))
+            {
+                let allText = rawFile.responseText;
+
+                let parser = new DOMParser();
+                let htmlDoc = parser.parseFromString(allText, 'text/html');
+
+                let divElement = document.createElement("div");
+
+                let sectionNodes = htmlDoc.querySelectorAll("section");
+                sectionNodes.forEach(function (sectionNode)
+                {
+                    divElement.appendChild(sectionNode);
+                });
+
+                container.appendChild(divElement);
+            }
+        }
+
+        rawFile.send(null);
+    }
+}
